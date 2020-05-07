23b29d5e6a2e68a40e5e01e1fa5a6e9225a102c8
sudhanshu <sudhanshusoni500@gmail.com>
2020-05-07 20:01:52 +0530

    use intent to open other actitvties

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,8 +15,10 @@
  */
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,5 +28,25 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+    }
+
+    public void openNumberList(View view){
+        Intent i = new Intent(this , NumbersActivity.class);
+        startActivity(i);
+    }
+
+    public void openColorList(View view){
+        Intent i = new Intent(this , ColorsActivity.class);
+        startActivity(i);
+    }
+
+    public void openFamilyList(View view){
+        Intent i = new Intent(this , FamilyActivity.class);
+        startActivity(i);
+    }
+
+    public void openPhrasesList(View view){
+        Intent i = new Intent(this , PhrasesActivity.class);
+        startActivity(i);
     }
 }
