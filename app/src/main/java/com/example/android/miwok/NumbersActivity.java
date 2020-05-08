a5501d4c690ed5f4cdf5a3be24b0a4a6e81119f0
sudhanshu <sudhanshusoni500@gmail.com>
2020-05-08 09:57:11 +0530

    Add log messages for the words array in NumbersActivity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,7 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -22,5 +23,18 @@ public class NumbersActivity extends AppCompatActivity {
         words[7] = "eight";
         words[8] = "nine";
         words[9] = "ten";
+
+        //Add log messages for the words array in NumbersActivity
+        Log.v("NumberActivity", "Word at index 0: "+ words[0]);
+        Log.v("NumberActivity", "Word at index 1: "+ words[1]);
+        Log.v("NumberActivity", "Word at index 2: "+ words[2]);
+        Log.v("NumberActivity", "Word at index 3: "+ words[3]);
+        Log.v("NumberActivity", "Word at index 4: "+ words[4]);
+        Log.v("NumberActivity", "Word at index 5: "+ words[5]);
+        Log.v("NumberActivity", "Word at index 6: "+ words[6]);
+        Log.v("NumberActivity", "Word at index 7: "+ words[7]);
+        Log.v("NumberActivity", "Word at index 8: "+ words[8]);
+        Log.v("NumberActivity", "Word at index 9: "+ words[9]);
+
     }
 }
